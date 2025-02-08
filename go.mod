14d62147ca9924b0e9c11606a7ea1030aa389c29
Oleh <sagiroleg@gmail.com>
2025-02-08 19:55:02 +0100

    DB added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module quizlet_bot
+
+go 1.23.3
---
+++ schema/000001_init..sql
@@ -0,0 +1,34 @@
+create table if not exists users(
+    id serial primary key unique,
+    chat_id integer,
+    name varchar(50) not null
+);
+
+create table if not exists topics(
+    id serial primary key unique,
+    topic varchar(20) not null,
+    user_id  integer references users(id) on delete cascade
+);
+
+create table if not exists words(
+    word varchar(50) not null,
+    translate varchar(50) not null,
+    topic_id integer references topics(id) on delete cascade
+);
+
+create table if not exists stats(
+    id serial primary key unique,
+    user_id integer references users(id) on delete cascade,
+    topic_id integer references topics(id) on delete cascade,
+    stats integer
+);
+
+---- create above / drop below ----
+
+drop table if exists users;
+
+drop table  if exists topics;
+
+drop table if exists words;
+
+drop table if exists stats;
